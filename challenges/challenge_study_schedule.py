3eb97f61fdcb65a89ad5b18bcfbf73e546c6e5f0
Thiago Ferreira <thiagocristhian@gmail.com>
2021-09-15 16:19:26 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    """criando a PR"""
+    counter = 0
+    if not (isinstance(target_time, int) and target_time >= 0):
+        return None
+    for period in permanence_period:
+        if not (
+            isinstance(period, tuple)
+            and isinstance(period[0], int)
+            and isinstance(period[1], int)
+        ):
+            return None
+        if period[0] <= target_time and period[1] >= target_time:
+            counter += 1
+    return counter
