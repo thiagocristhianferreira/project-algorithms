b65d92f650d1a3d3b326338f1a72cc07e4298cb0
Thiago Ferreira <thiagocristhian@gmail.com>
2021-09-15 19:23:14 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    reverse_word = ''
+    for letter in (range(len(word) - 1, - 1, - 1)):
+        reverse_word += word[letter]
+    if reverse_word == word.upper():
+        return True
+    return False
