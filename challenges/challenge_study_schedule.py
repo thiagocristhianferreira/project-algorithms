01f2bc60a5187d9044a9c6007813e15f7b51ebd9
Thiago Ferreira <thiagocristhian@gmail.com>
2021-09-15 17:39:00 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    counter = 0
-    if not (isinstance(target_time, int) and target_time >= 0):
+    try:
+        counter = 0
+        for period in permanence_period:
+            if (period[0] <= target_time <= period[1]):
+                counter += 1
+        return counter
+    except TypeError:
         return None
-    for period in permanence_period:
-        if not (
-            isinstance(period[0], int)
-            and isinstance(period[1], int)
-        ):
-            return None
-        if period[0] <= target_time <= period[1]:
-            counter += 1
-    return counter
