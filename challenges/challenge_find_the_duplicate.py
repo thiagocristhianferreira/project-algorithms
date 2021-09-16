1f123f25d267e3028e4fd8fe60f67aa853749e92
Thiago Ferreira <thiagocristhian@gmail.com>
2021-09-16 16:37:49 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not (isinstance(nums, list)):
+        return False
+    nums.sort()
+    repeats = 0
+    for i in range(len(nums)-1):
+        if isinstance(nums[i], str) or nums[i] < 0:
+            return False
+        if nums[i] == nums[i+1]:
+            repeats += 1
+            return nums[i]
+    return repeats_verify(repeats)
+
+
+def repeats_verify(repeats):
+    if repeats == 0:
+        return False
